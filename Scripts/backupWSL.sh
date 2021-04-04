69f3070fc2bae32880a9aa8d4e3988a560fc8a61
Christen Lofland <ChristenLofland@gmail.com>
2021-04-03 20:01:53 -0500

    Improve Dropbox bakup script and exclude more things.

--- Scripts/backupWSL.sh
+++ Scripts/backupWSL.sh
@@ -13,6 +13,7 @@ UNISON_ARGUMENTS+=(-auto)
 UNISON_ARGUMENTS+=(-links false)
 UNISON_ARGUMENTS+=(-ignore "Name node_modules")
 UNISON_ARGUMENTS+=(-ignore "Name .nvm")
+UNISON_ARGUMENTS+=(-ignore "Name .zsh_history")
 UNISON_ARGUMENTS+=(-ignore "Name unison.log")
 UNISON_ARGUMENTS+=(-ignore "Name .cache")
 UNISON_ARGUMENTS+=(-ignore "Name .keychain")
@@ -20,8 +21,13 @@ UNISON_ARGUMENTS+=(-ignore "Name .npm")
 UNISON_ARGUMENTS+=(-ignore "Name .unison*")
 UNISON_ARGUMENTS+=(-ignore "Name .docker")
 UNISON_ARGUMENTS+=(-ignore "Name .ros")
+UNISON_ARGUMENTS+=(-ignore "Name .idea")
+UNISON_ARGUMENTS+=(-ignore "Name .git")
+UNISON_ARGUMENTS+=(-ignore "Name laravel-api/vendor")
+UNISON_ARGUMENTS+=(-ignore "Name laravel-api/storage")
 UNISON_ARGUMENTS+=(-ignore "Name .oh-my-zsh") # This is entirely pulled from git
 UNISON_ARGUMENTS+=(-ignore "Name .tmux")      # This is entirely pulled from git
+UNISON_ARGUMENTS+=(-batch)                    # Causes it to propagate changes without stopping to ask first.
 
 if [[ -d /mnt/c/Users/chris/Dropbox/BACKUPS/WSL2-Linux ]]; then
   cp /mnt/c/Users/*/AppData/Local/Packages/Microsoft.WindowsTerminal*/LocalState/settings.json /mnt/c/Users/chris/Dropbox/BACKUPS/WSL2-Linux/
