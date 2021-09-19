94790138536b2223fca8a503ed5d959cec32ba65
Christen Lofland <ChristenLofland@gmail.com>
2021-09-19 16:20:20 -0500

    Improve Obsidian index text copy for Dropbox script.

--- Scripts/makeIndexableCopyOfObsidianMdFilesForDropbox.sh
+++ Scripts/makeIndexableCopyOfObsidianMdFilesForDropbox.sh
@@ -10,29 +10,57 @@
 PURPLE='\033[0;35m'
 NC='\033[0m' # NoColor
 
-OBSIDIAN_FOLDER="${HOME}"/Dropbox/Obsidian/
-MARKDOWN_FOLDER="${OBSIDIAN_FOLDER}"The\ Library\ of\ Babel/
-TEXT_INDEX_FOLDER="${OBSIDIAN_FOLDER}"Obsidian\ Text\ Index\ for\ Dropbox
+OBSIDIAN_FOLDER="${HOME}"/Dropbox/Obsidian
+MARKDOWN_FOLDER="${OBSIDIAN_FOLDER}"/The\ Library\ of\ Babel
+TEXT_INDEX_FOLDER="${OBSIDIAN_FOLDER}"/Obsidian\ Text\ Index\ for\ Dropbox
 
 if [[ -d "${MARKDOWN_FOLDER}" && -d "${TEXT_INDEX_FOLDER}" ]]; then
   printf "\n${PURPLE}Updating .txt copies of Obsidian files for Dropbox${NC}\n"
   cd "${MARKDOWN_FOLDER}" || exit
-  # First copy the .md files to the folder.
-  # https://stackoverflow.com/a/15617049/4982408
-  find . -name '*.md' -print0 | xargs -0 cp -t "${TEXT_INDEX_FOLDER}"
-  # Pause to let Dropbox/Windows deal with reality
-  sleep 1
-  # Then rename them to .txt
-  # TODO: In a perfect world this should walk the files, compare checksums,
-  # and only update files if they changed, so that Dropbox doesn't get
-  # all of these spurious updates.
-  cd "${TEXT_INDEX_FOLDER}" || exit
-  rm ./*.txt
-  # https://stackoverflow.com/a/44965562/4982408
-  # NOTE: File names have SPACES!
-  find . -name '*.md' -exec bash -c 'mv "$0" "${0/md/txt}"' {} \;
 
-  printf "\n${PURPLE}Committing Obsidian note changes to git${NC}\n"
-  cd "${OBSIDIAN_FOLDER}" || exit
-  git add -A && git commit -m "$(date)"
+  CHANGES_FOUND=0
+
+  # https://stackoverflow.com/a/9612232/4982408
+  shopt -s globstar
+  for i in **/*.md; do # Whitespace-safe and recursive
+    TEXT_FILE_NAME="${i/.md/.txt}"
+    INDEX_FILE_PATH="${TEXT_INDEX_FOLDER}/$(basename "${TEXT_FILE_NAME}")"
+    LOCAL_CHANGES_FOUND=0
+
+    if ! [[ -e "${INDEX_FILE_PATH}" ]]; then
+      LOCAL_CHANGES_FOUND=1
+    else
+      if [[ $(diff "${i}" "${INDEX_FILE_PATH}") ]]; then
+        LOCAL_CHANGES_FOUND=1
+      fi
+    fi
+
+    if [[ "${LOCAL_CHANGES_FOUND}" == "1" ]]; then
+      CHANGES_FOUND=1
+      echo "${i}"
+      cp "${i}" "${INDEX_FILE_PATH}"
+    fi
+  done
+
+  # Clean up .txt files left after .md file deletion
+  cd "${TEXT_INDEX_FOLDER}" || exit
+  # https://stackoverflow.com/a/9612232/4982408
+  shopt -s globstar
+  for i in **/*.txt; do # Whitespace-safe and recursive
+    MD_FILE_NAME="${i/.txt/.md}"
+    cd "${MARKDOWN_FOLDER}" || exit
+    if (($(find . -name "${MD_FILE_NAME}" | wc -l) == 0)); then
+      CHANGES_FOUND=1
+      echo "${i}"
+      cd "${TEXT_INDEX_FOLDER}" || exit
+      rm "${i}"
+    fi
+  done
+
+  if [[ "${CHANGES_FOUND}" == "1" ]]; then
+    cd "${OBSIDIAN_FOLDER}" || exit
+    git add -A
+    printf "\n${PURPLE}Committing Obsidian note changes to git${NC}\n"
+    git commit -m "$(date)"
+  fi
 fi
