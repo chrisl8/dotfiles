0ddee5f0503e4122520a60c2cb0ce6376602ffd1
Christen Lofland <Christen@Lofland.net>
2022-10-27 07:30:27 -0500

    Update panic stations sync. Raspbian now sets up a user account.

--- Scripts/syncPanicStations.sh
+++ Scripts/syncPanicStations.sh
@@ -3,7 +3,7 @@ REMOTE_IP=192.168.1.22
 
 UNISON_ARGUMENTS=()
 UNISON_ARGUMENTS+=(/home/chrisl8/Dev/PanicStations)
-UNISON_ARGUMENTS+=(ssh://pi@"${REMOTE_IP}"//home/pi/PanicStations)
+UNISON_ARGUMENTS+=(ssh://"${REMOTE_IP}"//home/chrisl8/PanicStations)
 #UNISON_ARGUMENTS+=(-ignore "Path node/node_modules") # Since I cannot install this locally, syncing them back is the best way to let IntelliJ see them
 UNISON_ARGUMENTS+=(-ignore "Path .idea")
 UNISON_ARGUMENTS+=(-auto)
@@ -12,4 +12,4 @@ if unison "${UNISON_ARGUMENTS[@]}"; then
   unison "${UNISON_ARGUMENTS[@]}" -ignore "Path node/node_modules" -batch -repeat watch
 fi
 
-scp pi@${REMOTE_IP}:/home/pi/PanicStations/node/statistics.csv "${HOME}"/Dropbox/PanicStations.csv
+#scp ${REMOTE_IP}:/home/chrisl8/PanicStations/node/statistics.csv "${HOME}"/Dropbox/PanicStations.csv
