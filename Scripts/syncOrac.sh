563e2f851df9e73f7d7bcf74c580fd824e125704
Christen Lofland <christen@lofland.net>
2023-03-29 17:16:56 -0500

    Update orac sync script.

--- Scripts/syncOrac.sh
+++ Scripts/syncOrac.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-REMOTE_IP=192.168.1.123
+REMOTE_IP=192.168.1.152
 
 UNISON_ARGUMENTS=()
 UNISON_ARGUMENTS+=(/home/chrisl8/Dev/Orac)
-UNISON_ARGUMENTS+=(ssh://"${REMOTE_IP}"//home/chrisl8/Orac)
+UNISON_ARGUMENTS+=(ssh://"mycroft@${REMOTE_IP}"//home/mycroft/Orac)
 UNISON_ARGUMENTS+=(-ignore "Path node/node_modules") # These are different on Pi and x86
 UNISON_ARGUMENTS+=(-ignore "Path .idea")
 UNISON_ARGUMENTS+=(-auto)
@@ -11,4 +11,3 @@ UNISON_ARGUMENTS+=(-auto)
 if unison "${UNISON_ARGUMENTS[@]}"; then
   unison "${UNISON_ARGUMENTS[@]}" -batch -repeat watch
 fi
-
