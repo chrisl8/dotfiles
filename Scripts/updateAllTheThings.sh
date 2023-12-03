2be6d1c372e2b69fcef5ef3b4331583100fe8f90
Christen Lofland <christen@lofland.net>
2023-12-02 20:15:56 -0600

    Fix wsl binary name.

--- Scripts/updateAllTheThings.sh
+++ Scripts/updateAllTheThings.sh
@@ -128,9 +128,9 @@ if (command -v wsl.exe > /dev/null); then
   # https://github.com/microsoft/WSL/issues/8603#issuecomment-1357669697
   printf "\n${BRIGHT_MAGENTA}WSL Updates${NC}\n"
   printf "NOTE: If this just terminates Linux or otherwise fails, you may need to open PowerShell AS ADMINISTRATOR and run:\n"
-  printf "wsl --update\n"
+  printf "wsl.exe --update\n"
   printf "or if that fails try:\n"
-  printf "wsl --update --web-download\n\n"
+  printf "wsl.exe --update --web-download\n\n"
   wsl.exe --update || true
   wsl.exe --status
 fi
