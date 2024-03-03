68486e00030ebb78f62b1f1416effbf65405dbbe
Christen Lofland <christen@lofland.net>
2024-03-02 19:22:21 -0600

    Use a full path to python.

--- Scripts/updateAllTheThings.sh
+++ Scripts/updateAllTheThings.sh
@@ -59,7 +59,7 @@ sudo apt -y autoremove
 if (command -v pip >/dev/null) && [[ -e ${HOME}/.local/bin/gdformat ]]; then
   printf "\n${BRIGHT_MAGENTA}Python Updates${NC}\n"
   printf "\n${LIGHTBLUE}Updating PIP${NC}\n"
-  sudo python -m pip install --upgrade pip
+  sudo /usr/bin/python -m pip install --upgrade pip
   printf "\n${LIGHTBLUE}Install/Updating Python Packages${NC}\n"
   pip install "gdtoolkit==4.*" --upgrade
 fi
