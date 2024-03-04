e6131de035bb31fd4d8486d36d03d6c9a0626d14
Christen Lofland <christen@lofland.net>
2024-03-04 17:14:15 -0600

    Display change list when updating Godot build from source.

--- Scripts/godotBuildLinux.sh
+++ Scripts/godotBuildLinux.sh
@@ -9,7 +9,7 @@ YELLOW='\033[1;33m'
 LIGHTBLUE='\033[1;34m'
 NC='\033[0m' # NoColor
 
-printf "\n${YELLOW}Building latest Godot version from GitHub.${NC}\n"
+printf "\n${YELLOW}Checking latest Godot version from GitHub.${NC}\n"
 
 # Kind of assuming if clong and lld exist, the rest do.
 if ! (command -v clang >/dev/null) || ! (command -v lld >/dev/null); then
@@ -35,9 +35,12 @@ if [[ -d "godot" ]]; then
   cd godot || exit
   git fetch
   LOCAL=$(git rev-parse @)
-  REMOTE=$(git rev-parse @{u})
+  REMOTE=$(git rev-parse "@{u}")
   if [[ "$LOCAL" != "$REMOTE" ]]; then
     REPO_UPDATED="True"
+    printf "${LIGHTBLUE}Found the following updates:${NC}\n"
+    git --no-pager log --oneline ^"$LOCAL" "$REMOTE"
+    printf "\n"
     git pull
   fi
 else
