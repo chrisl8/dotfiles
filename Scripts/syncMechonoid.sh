6a1782e4d272bc73b93c135fa3b2923008e7c667
Christen Lofland <christen@lofland.net>
2024-03-12 22:14:30 -0500

    Update Mechonoid syncer to use WSL2 based ssh/scp to work with 1Password.

--- Scripts/syncMechonoid.sh
+++ Scripts/syncMechonoid.sh
@@ -12,7 +12,7 @@ if [[ "${REMOTE_IP}" == "" ]]; then
 fi
 
 function copyDotFiles() {
-  unison "${HOME}"/Dev/DotfilesForMechonoid/"${REMOTE_IP}" ssh://"${USER}"@"${REMOTE_IP}"//home/"${USER}" -path .mechonoid -auto
+  unison "${HOME}"/Dev/DotfilesForMechonoid/"${REMOTE_IP}" ssh://"${USER}"@"${REMOTE_IP}"//home/"${USER}" -path .mechonoid -sshcmd "ssh.exe" -auto
 }
 
 copyDotFiles
@@ -25,11 +25,12 @@ UNISON_ARGUMENTS+=(-ignore "Name .idea")
 UNISON_ARGUMENTS+=(-ignore "Name node/node_modules")
 UNISON_ARGUMENTS+=(-ignore "Name website/node_modules")
 UNISON_ARGUMENTS+=(-auto)
+UNISON_ARGUMENTS+=(-sshcmd "ssh.exe")
 
 if unison "${UNISON_ARGUMENTS[@]}"; then
   # pigpio cannot install on a non-Pi box, but we need it for IntelliJ to code complete:
   if [[ -d "${HOME}"/Dev/Mechonoid/node/node_modules/ ]]; then
-    scp -r "${USER}"@"${REMOTE_IP}":/home/"${USER}"/Mechonoid/node/node_modules/pigpio "${HOME}"/Dev/Mechonoid/node/node_modules/ >/dev/null
+    scp.exe -r "${USER}"@"${REMOTE_IP}":/home/"${USER}"/Mechonoid/node/node_modules/pigpio "${HOME}"/Dev/Mechonoid/node/node_modules/ >/dev/null
   fi
 
   unison "${UNISON_ARGUMENTS[@]}" -batch -repeat watch
